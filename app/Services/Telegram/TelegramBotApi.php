50c73a802441120cd6f2c5c5acf392d9a0b5aa15
AntonBelousGit <antonbelouswork@gmail.com>
2022-10-15 15:16:02 +0300

    Improve code

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -10,21 +10,30 @@ use Illuminate\Support\Facades\Log;
 
 final class TelegramBotApi
 {
+    /**
+     * Telegram API host
+     */
     public const HOST = 'https://api.telegram.org/bot';
 
     /**
+     * Send message via telegram
      * @param string $message
      */
     public static function sendMessage(string $message): void
     {
         try {
-            Http::get(
-                self::HOST . config('telegram.token') . '/sendMessage',
+            $result = Http::get(
+                self::HOST . config('telegram.token') . '/1sendMessage',
                 [
                     'chat_id' => config('telegram.chatId'),
                     'text' => $message
                 ]
             );
+
+            if ($result->getStatusCode() !== 200) {
+                $errors = $result->getBody()->getContents();
+                Log::info(['error' => $errors, 'from' => 'App\Services\Telegram\TelegramBotApi']);
+            }
         } catch (Exception $e) {
             Log::info(['error' => $e, 'from' => 'App\Services\Telegram\TelegramBotApi']);
         }
